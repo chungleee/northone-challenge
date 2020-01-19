a8f99dfa72f114347345168a26d8a376d61e4a8c
Leon <leon.karho.chung@gmail.com>
2020-01-19 17:17:35 -0500

    remove dummy data and receive them from props instead

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,29 +1,6 @@
 import React from "react";
 
-const dummies = [
-  {
-    id: 1,
-    title: "groceries",
-    description: "do groceries",
-    status: "pending",
-    due_date: "january 20th, 2020"
-  },
-  {
-    id: 2,
-    title: "gas",
-    description: "fill gas",
-    status: "pending",
-    due_date: "january 20th, 2020"
-  },
-  {
-    id: 3,
-    title: "charge phone",
-    description: "need phone battery to work bruh",
-    status: "pending",
-    due_date: "january 20th, 2020"
-  }
-];
-const TodoList = () => {
+const TodoList = ({ dummies }) => {
   return (
     <ul className="list pa2">
       {dummies.map(dummy => {
