209e90f8c715cd7e03d6a4c8498c466cbf763a93
Leon <leon.karho.chung@gmail.com>
2020-01-19 17:16:22 -0500

    set dummy state, create todo handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import CreateTodo from "./pages/CreateTodo";
 import TodoList from "./pages/TodoList";
 
 const App = () => {
+  const [dummies, setDummies] = useState([
+    {
+      id: 1,
+      title: "groceries",
+      description: "do groceries",
+      status: "pending",
+      due_date: "january 20th, 2020"
+    },
+    {
+      id: 2,
+      title: "gas",
+      description: "fill gas",
+      status: "pending",
+      due_date: "january 20th, 2020"
+    },
+    {
+      id: 3,
+      title: "charge phone",
+      description: "need phone battery to work bruh",
+      status: "pending",
+      due_date: "january 20th, 2020"
+    }
+  ]);
+
+  const handleCreateTodo = todo => {
+    setDummies(state => {
+      return [...state, todo];
+    });
+  };
+
   return (
     <Router>
       <div>
@@ -14,14 +44,14 @@ const App = () => {
         exact
         path="/"
         render={props => {
-          return <TodoList {...props} />;
+          return <TodoList dummies={dummies} {...props} />;
         }}
       />
 
       <Route
         path="/create"
         render={props => {
-          return <CreateTodo {...props} />;
+          return <CreateTodo handleCreateTodo={handleCreateTodo} {...props} />;
         }}
       />
 
