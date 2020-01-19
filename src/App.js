3039621d7accab351f965bcce3cfef92693b1f78
Leon <leon.karho.chung@gmail.com>
2020-01-19 16:12:28 -0500

    setup Router, Routes and Link button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import CreateTodo from "./pages/CreateTodo";
+import TodoList from "./pages/TodoList";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="f1 tc">NorthOne Front End Challenge</h1>
-    </div>
+    <Router>
+      <div>
+        <h1 className="f2 tc ph4">NorthOne Front End Challenge</h1>
+      </div>
+
+      <Route
+        exact
+        path="/"
+        render={props => {
+          return <TodoList {...props} />;
+        }}
+      />
+
+      <Route
+        path="/create"
+        render={props => {
+          return <CreateTodo {...props} />;
+        }}
+      />
+
+      <Link
+        className="link black"
+        style={{ position: "absolute", bottom: "1rem", right: "1rem" }}
+        to="/create"
+      >
+        <div className="ba br-100 h3 w3 flex justify-center items-center">
+          +
+        </div>
+      </Link>
+    </Router>
   );
 };
 
