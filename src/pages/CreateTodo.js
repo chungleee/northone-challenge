53e615023c194e07ec0186321f2056ef8689d105
Leon <leon.karho.chung@gmail.com>
2020-01-19 16:54:15 -0500

    create todo form with formik, import common reusable components

--- src/pages/CreateTodo.js
+++ src/pages/CreateTodo.js
@@ -1,7 +1,44 @@
 import React from "react";
+import { Formik } from "formik";
+import InputField from "../common/InputField";
+import TextArea from "../common/TextArea";
 
+const initialValues = {
+  title: "",
+  description: "",
+  status: "pending",
+  due_date: ""
+};
 const CreateTodo = params => {
-  return <div>create todo page</div>;
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        console.log("values", values);
+        actions.setSubmitting(false);
+      }}
+    >
+      {props => {
+        return (
+          <form onSubmit={props.handleSubmit}>
+            <InputField
+              label="Title"
+              type="text"
+              name="title"
+              onChange={props.handleChange}
+            />
+            <TextArea
+              label="Description"
+              type="text"
+              name="description"
+              onChange={props.handleChange}
+            />
+            <button type="submit">Save</button>
+          </form>
+        );
+      }}
+    </Formik>
+  );
 };
 
 export default CreateTodo;
