37f403b401537c4a5efaefe24598aa8dd4801374
Leon <leon.karho.chung@gmail.com>
2020-01-19 18:19:25 -0500

    set up datepicker component

--- src/pages/CreateTodo.js
+++ src/pages/CreateTodo.js
@@ -3,12 +3,14 @@ import { Formik } from "formik";
 import InputField from "../common/InputField";
 import TextArea from "../common/TextArea";
 import { Link } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.min.css";
 
 const initialValues = {
   title: "",
   description: "",
   status: "pending",
-  due_date: ""
+  due_date: new Date()
 };
 const CreateTodo = ({ handleCreateTodo, history }) => {
   return (
@@ -19,12 +21,13 @@ const CreateTodo = ({ handleCreateTodo, history }) => {
       <Formik
         initialValues={initialValues}
         onSubmit={(values, actions) => {
+          console.log("values", values);
           handleCreateTodo(values);
           actions.setSubmitting(false);
           history.push("/");
         }}
       >
-        {({ handleSubmit, handleChange }) => {
+        {({ handleSubmit, handleChange, setFieldValue, values }) => {
           return (
             <form onSubmit={handleSubmit}>
               <InputField
@@ -39,6 +42,21 @@ const CreateTodo = ({ handleCreateTodo, history }) => {
                 name="description"
                 onChange={handleChange}
               />
+              <div>
+                <label htmlFor="due_date" className="f4 b db mb2">
+                  Set due date
+                </label>
+                <DatePicker
+                  name="due_date"
+                  selected={values.due_date}
+                  onChange={date => setFieldValue("due_date", date)}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={30}
+                  timeCaption="time"
+                  dateFormat="MMMM d, yyyy h:mm aa"
+                />
+              </div>
               <button type="reset">Reset</button>
               <button type="submit">Save</button>
             </form>
