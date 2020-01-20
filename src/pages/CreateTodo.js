e087e77daa018b253aaeabac998ebab2d7f7e59c
Leon <leon.karho.chung@gmail.com>
2020-01-20 10:20:27 -0500

    setup local tasks state, create todo obj to include tasks arr and values, list tasks after adding

--- src/pages/CreateTodo.js
+++ src/pages/CreateTodo.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik } from "formik";
 import InputField from "../common/InputField";
 import TextArea from "../common/TextArea";
 import { Link } from "react-router-dom";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.min.css";
+import uuid from "uuid/v4";
 
 const initialValues = {
   title: "",
   description: "",
+  task: "",
   status: "pending",
   due_date: new Date()
 };
 const CreateTodo = ({ handleCreateTodo, history }) => {
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div>
       <div>
@@ -22,8 +26,11 @@ const CreateTodo = ({ handleCreateTodo, history }) => {
         initialValues={initialValues}
         onSubmit={(values, actions) => {
           console.log("values", values);
-          handleCreateTodo(values);
-          actions.setSubmitting(false);
+          const todo = {
+            tasks,
+            ...values
+          };
+          handleCreateTodo(todo);
           history.push("/");
         }}
       >
@@ -42,6 +49,36 @@ const CreateTodo = ({ handleCreateTodo, history }) => {
                 name="description"
                 onChange={handleChange}
               />
+              <div>
+                <InputField
+                  label="Add some tasks"
+                  type="text"
+                  value={values.task}
+                  name="task"
+                  onChange={handleChange}
+                />
+                <input
+                  type="button"
+                  value="Add"
+                  onClick={() => {
+                    const newTask = {
+                      id: uuid(),
+                      task: values.task
+                    };
+                    setTasks(state => {
+                      return [...state, newTask];
+                    });
+                    setFieldValue("task", "");
+                  }}
+                />
+              </div>
+              {tasks.length > 0 ? (
+                <ul>
+                  {tasks.map(task => {
+                    return <li key={task.id}>{task.task}</li>;
+                  })}
+                </ul>
+              ) : null}
               <div>
                 <label htmlFor="due_date" className="f4 b db mb2">
                   Set due date
