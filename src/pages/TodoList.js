3023f3f5739b02e6ce5c0cefbfc43428eea6d456
Leon <leon.karho.chung@gmail.com>
2020-01-19 19:48:39 -0500

    render according to loading or if todos is populated

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const TodoList = ({ dummies }) => {
+const TodoList = ({ todos, loading }) => {
   return (
     <ul className="list pa2">
-      {dummies.map(dummy => {
-        return (
-          <li className="ba br3 mb1" key={dummy.id}>
-            <h3 className="f4 ttc">{dummy.title}</h3>
-          </li>
-        );
-      })}
+      {loading ? (
+        <div>Loading...</div>
+      ) : !todos.length ? (
+        <div>Todo List is empty!</div>
+      ) : (
+        todos.map(todo => {
+          return (
+            <li className="ba br3 mb1" key={todo._id}>
+              <h3 className="f4 ttc">{todo.title}</h3>
+            </li>
+          );
+        })
+      )}
       <Link
         className="link black"
         style={{ position: "absolute", bottom: "1rem", right: "1rem" }}
