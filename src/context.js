9c694e210afb0618aa93c6cc9ce921a59d4a9d6f
Leon <leon.karho.chung@gmail.com>
2020-01-20 17:39:16 -0500

    init context & provider and import states and handlers

---
+++ src/context.js
@@ -0,0 +1,94 @@
+import React, { createContext, useState } from "react";
+import db from "./database";
+const TodoContext = createContext();
+
+const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(null);
+
+  const handleCreateTodo = async todo => {
+    try {
+      const response = await db.post(todo);
+      const newTodo = await db.get(response.id);
+      setTodos(state => {
+        return [...state, newTodo];
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleGetTodos = async () => {
+    try {
+      setLoading(true);
+      const res = await db.allDocs({ include_docs: true });
+      const todos = res.rows.map(row => {
+        return row.doc;
+      });
+      setLoading(false);
+      setTodos(todos);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleEditTodo = async (_rev, _id, editedTodo) => {
+    try {
+      const response = await db.put({
+        _rev,
+        _id,
+        ...editedTodo
+      });
+      const updatedTodo = await db.get(response.id);
+      const updatedList = todos.map(todo => {
+        if (todo._id === updatedTodo._id) {
+          return updatedTodo;
+        }
+        return todo;
+      });
+      setTodos(updatedList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteTodo = async (_id, _rev) => {
+    try {
+      const response = await db.remove(_id, _rev);
+      const updatedList = todos.filter(todo => {
+        if (todo._id !== response.id) {
+          return todo;
+        }
+      });
+      setTodos(updatedList);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleToggleTask = async (_id, _rev, data) => {
+    try {
+      const response = await db.put({ _id, _rev, data });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <TodoContext.Provider
+      value={{
+        loading,
+        todos,
+        handleGetTodos,
+        handleCreateTodo,
+        handleEditTodo,
+        handleDeleteTodo,
+        handleToggleTask
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export { TodoContext, TodoProvider };
