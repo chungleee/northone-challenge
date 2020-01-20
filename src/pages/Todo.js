65a45b7ace5f7deda1f1533b51144aaae03444a5
Leon <leon.karho.chung@gmail.com>
2020-01-20 10:31:17 -0500

    fi xbug

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 
 const Todo = ({ location, match }) => {
   const { todo } = location.state;
+  console.log("todo", todo);
   const { url } = match;
   return (
     <div>
       <h1 className="f3">{todo.title}</h1>
       <p>{todo.description}</p>
-      {todo.task !== undefined && todo.tasks.length > 0 ? (
+      {todo.tasks !== undefined && todo.tasks.length > 0 ? (
         <ul>
           {todo.tasks.map(task => {
             return <li key={task.id}>{task.task}</li>;
