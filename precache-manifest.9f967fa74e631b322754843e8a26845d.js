9e9db5e9e5853121be3b18fe90aa5436e7060209
Leon <leon.karho.chung@gmail.com>
2020-01-22 16:04:03 -0500

    Updates

---
+++ precache-manifest.9f967fa74e631b322754843e8a26845d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34392aa2c21ca7974888d2425faafeed",
+    "url": "/northone-challenge/index.html"
+  },
+  {
+    "revision": "ff09f5573c5b2ca8aa6c",
+    "url": "/northone-challenge/static/css/2.dfa2f52a.chunk.css"
+  },
+  {
+    "revision": "ff09f5573c5b2ca8aa6c",
+    "url": "/northone-challenge/static/js/2.c9e49488.chunk.js"
+  },
+  {
+    "revision": "0d51a26e357591260d29df3c0b1559d1",
+    "url": "/northone-challenge/static/js/2.c9e49488.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa0ee6fa9d23edf5365a",
+    "url": "/northone-challenge/static/js/main.2462238e.chunk.js"
+  },
+  {
+    "revision": "1b3f6e91df066f52db0b",
+    "url": "/northone-challenge/static/js/runtime-main.5996e1f7.js"
+  }
+]);
