22fc63a7de3e8f6eabda1a80b0a04897f9803fde
Leon <leon.karho.chung@gmail.com>
2020-01-22 14:50:57 -0500

    init input value state + filter todos by value

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,44 +1,60 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { TodoContext } from "../context";
 
 const TodoList = () => {
   const { todos, loading, handleDeleteTodo } = useContext(TodoContext);
+  const [value, setValue] = useState("");
+  const handleOnChange = event => {
+    setValue(event.target.value);
+  };
   return (
     <ul className="list pa2">
+      <input
+        onChange={event => {
+          handleOnChange(event);
+        }}
+        className="input-reset ba b--black-20 pa2 mb2 db w-100"
+        type="text"
+        aria-describedby="search box"
+      />
       {loading ? (
         <div className="tc">Loading...</div>
       ) : !todos.length ? (
         <div className="tc">Todo List is empty!</div>
       ) : (
-        todos.map(todo => {
-          return (
-            <li
-              className="flex justify-between items-center  ba br3 mb1 ph2 grow"
-              key={todo._id}
-            >
-              <Link
-                className="link black"
-                to={{
-                  pathname: `/todo/${todo._id}`,
-                  state: {
-                    todo
-                  }
-                }}
+        todos
+          .filter(todo => {
+            return todo.title.toLowerCase().includes(value.toLowerCase());
+          })
+          .map(todo => {
+            return (
+              <li
+                className="flex justify-between items-center  ba br3 mb1 ph2 grow"
+                key={todo._id}
               >
-                <h3 className="f4 ttc">{todo.title}</h3>
-              </Link>
-              <div>
-                <i
-                  onClick={() => {
-                    handleDeleteTodo(todo._id, todo._rev);
+                <Link
+                  className="link black"
+                  to={{
+                    pathname: `/todo/${todo._id}`,
+                    state: {
+                      todo
+                    }
                   }}
-                  className="far fa-trash-alt red pointer"
-                ></i>
-              </div>
-            </li>
-          );
-        })
+                >
+                  <h3 className="f4 ttc">{todo.title}</h3>
+                </Link>
+                <div>
+                  <i
+                    onClick={() => {
+                      handleDeleteTodo(todo._id, todo._rev);
+                    }}
+                    className="far fa-trash-alt red pointer"
+                  ></i>
+                </div>
+              </li>
+            );
+          })
       )}
       <Link
         className="link black"
